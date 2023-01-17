d4e8558017a74b1ff35e430b64f88e1a6f6f69b4
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-01-18 11:48:00 +1300

    Rack < v3 compatible release.

--- lib/rack/session.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Rack
-  module Session
-    autoload :Cookie, "rack/session/cookie"
-    autoload :Pool, "rack/session/pool"
-    autoload :Memcache, "rack/session/memcache"
-  end
-end
--- lib/rack/session/constants.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Rack
-  module Session
-    RACK_SESSION                        = 'rack.session'
-    RACK_SESSION_OPTIONS                = 'rack.session.options'
-    RACK_SESSION_UNPACKED_COOKIE_DATA   = 'rack.session.unpacked_cookie_data'
-  end
-end
