29cdbe35c1e229023e43d743659ce4ff8664620a
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2023-01-18 16:20:49 +1300

    Revert "Rack < v3 compatible release."
    This reverts commit d4e8558017a74b1ff35e430b64f88e1a6f6f69b4.

---
+++ lib/rack/session.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+module Rack
+  module Session
+    autoload :Cookie, "rack/session/cookie"
+    autoload :Pool, "rack/session/pool"
+    autoload :Memcache, "rack/session/memcache"
+  end
+end
