219d8da15b0d1a02c650f956df29db42408a6adb
John R McGarvey <johnmcgarvey55@gmail.com>
2024-03-06 15:21:24 -0500

    Introduce `assume_ssl` option to allow secure session cookies through insecure proxy (#41)

--- test/spec_session_abstract_persisted.rb
+++ test/spec_session_abstract_persisted.rb
@@ -68,4 +68,28 @@ describe Rack::Session::Abstract::Persisted do
   it "#delete_session raises" do
     proc { @pers.send(:delete_session, nil, nil, nil) }.must_raise RuntimeError
   end
+
+  describe '#security_matches?' do
+
+    it '#security_matches? returns true if secure cookie is off' do
+      @pers.send(:security_matches?, Rack::Request.new({}), {}).must_equal true
+    end
+
+    it '#security_matches? returns true if ssl is on' do
+      req = Rack::Request.new({})
+      req.set_header('HTTPS', 'on')
+      @pers.send(:security_matches?, req, { secure: true }).must_equal true
+    end
+
+    it '#security_matches? returns true if assume_ssl option is set' do
+      req = Rack::Request.new({})
+      pers_with_persist = @class.new(nil, { assume_ssl: true })
+      pers_with_persist.send(:security_matches?, req, { secure: true }).must_equal true
+    end
+
+    it '#security_matches? returns false if secure cookie is on, but not ssl or assume_ssl' do
+      @pers.send(:security_matches?, Rack::Request.new({}), { secure: true }).must_equal false
+    end
+
+  end
 end
